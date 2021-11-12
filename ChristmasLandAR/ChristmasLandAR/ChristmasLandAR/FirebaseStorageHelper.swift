3855a91d03b330020fc1f8d21b32319303904f62
Daniel <dannystvincent@gmail.com>
2021-11-11 23:10:30 -0500

    - Thumbnail images running from Firebase Storage -3D Asset running from firebase storage -Images are currently broken 😞 When app builds

---
+++ ChristmasLandAR/ChristmasLandAR/ChristmasLandAR/FirebaseStorageHelper.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseStorageHelper.swift
+//  ChristmasLandAR
+//
+//  Created by Daniel St Vincent on 11/11/21.
+//
+
+import Foundation
---
+++ ChristmasLandAR/ChristmasLandAR/ChristmasLandAR/ModelsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ModelsViewModel.swift
+//  ChristmasLandAR
+//
+//  Created by Daniel St Vincent on 11/10/21.
+//
+
+import Foundation
